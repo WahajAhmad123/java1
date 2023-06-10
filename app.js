d541512665eeab7a461d98a80f497ce7ee33566d
WahajAhmad123 <115481366+WahajAhmad123@users.noreply.github.com>
2023-06-10 12:51:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var a = prompt("Enter your Name");
+var b = prompt("Enter your Email");
+var c = prompt("Enter your Age");
+
+alert("name : " + " " + a + " " + " Email : " + " " + b + " " + " Age : " + " " + c );
